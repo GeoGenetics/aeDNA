4353b39a3685ff82eebc667f108f0bb1681f9ba6
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-12-18 13:18:48 +0100

    Add default adapters, tweaked logging, no longer skip datasets, and code reformat

--- make_units.py
+++ make_units.py
@@ -10,6 +10,21 @@ import pandas as pd
 from pathlib import Path
 
 
+
+def gzip_n_lines(in_gzip):
+    with gzip.open(in_gzip, "rb") as f:
+        for i, l in enumerate(f):
+            pass
+    return i+1
+
+
+
+# Default adapters
+adapters = {"ss": "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,GGAAGAGCGTCGTGTAGGGAAAGAGTGT",
+            "ds": "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"}
+
+
+
 # Parse command-line arguments
 parser = argparse.ArgumentParser(
     description="Generate `sample` and `unit` files from list of FASTQ files (on stdin).",
@@ -34,8 +49,14 @@ parser.add_argument(
     "--min-file-size",
     action="store",
     type=int,
-    default=50,
-    help="Minimum file size (KiB).",
+    default=20,
+    help="Minimum file size (KiB) for detailed file info.",
+)
+parser.add_argument(
+    "--rm-chars",
+    action="store",
+    default=r"[\s\-_\/\\]",
+    help="Invalid characters (will be removed).",
 )
 parser.add_argument(
     "-c",
@@ -48,24 +69,24 @@ parser.add_argument(
 parser.add_argument(
     "--library-type",
     action="store",
-    default="ss",
+    default=np.nan,
     choices=["ss", "ds"],
     help="Library type.",
 )
 parser.add_argument(
-    "--rm-chars",
+    "--adapters",
     action="store",
-    default=r"[\s\-_\/\\]",
-    help="Invalid characters (will be removed).",
+    default=np.nan,
+    help="Adapters (comma-separated).",
 )
 parser.add_argument(
-    "--no-header-info",
+    "--metadata-ignore-header",
     action="store_true",
     default=False,
     help="Do not parse info from read header.",
 )
 parser.add_argument(
-    "--extra-metadata",
+    "--metadata-extra",
     action="store",
     nargs="+",
     default=["sample=Lib", "center=CAEG", "platform=ILLUMINA", "material=DNA"],
@@ -94,7 +115,7 @@ parser.add_argument(
     "-l",
     "--loglevel",
     action="store",
-    default="INFO",
+    default="WARNING",
     choices=["DEBUG", "INFO", "WARNING", "ERROR"],
     help="Log verbosity level",
 )
@@ -110,32 +131,35 @@ args = parser.parse_args()
 if not args.fq_files:
     args.fq_files = [fq_file.strip() for fq_file in sys.stdin.readlines()]
 print(f"# {args}")
-units = pd.DataFrame()
 
 
+# Adding metadata to extra
+if args.library_type and np.isnan(args.adapters):
+    args.adapters = adapters.get(args.library_type, np.nan)
+for key in ["library_type", "adapters"]:
+    if value := getattr(args, key):
+        args.metadata_extra.append(f"{key}={value}")
+
+
+# Set logger
 loglevel = getattr(logging, args.loglevel.upper(), None)
 logging.basicConfig(encoding="utf-8", level=loglevel)
 logging.info(f"Found {len(args.fq_files)} input files")
 
 
+units = pd.DataFrame()
 for fq_file in sorted(args.fq_files):
     row = dict()
     fq_file = Path(fq_file.rstrip())
     row["data"] = str(fq_file.resolve(strict=True))
     # Get info from read IDs
-    if not args.no_header_info:
+    if not args.metadata_ignore_header:
         with gzip.open(row["data"], "rt") as gz:
-            read_id = gz.readline().lstrip("@").rstrip().split(":")
-            row["machine"] = read_id[0]
-            row["run_n"] = read_id[1]
-            row["flowcell"] = read_id[2]
-    # Get info from arguments
-    row["library_type"] = args.library_type
-    for extra_metadata in args.extra_metadata:
-        key, value = extra_metadata.split("=")
-        row[key] = value
-    row["seq_type"] = "SE"
-    row["adapters"] = np.nan
+            read_header = gz.readline().lstrip("@").rstrip().split(":")
+            if len(read_header) > 2:
+                row["machine"] = read_header[0]
+                row["run_n"] = read_header[1]
+                row["flowcell"] = read_header[2]
     # Get info from file name
     fq_prefix = (
         fq_file.with_suffix("").with_suffix("")
@@ -164,6 +188,13 @@ for fq_file in sorted(args.fq_files):
     row = pd.DataFrame([row])
     logging.debug(row)
     units = pd.concat([units, row])
+
+
+# Add metadata
+units["seq_type"] = "SE"
+for metadata_extra in args.metadata_extra:
+    key, value = metadata_extra.split("=")
+    units[key] = value
 
 
 # Format date column (if present)
@@ -204,7 +235,7 @@ units.sort_values(by=list(units.columns.values), inplace=True)
 # Fix invalid values
 fix_cols = units.columns.drop("data")
 units[fix_cols] = units[fix_cols].replace(args.rm_chars, value="", regex=True)
-logging.info(units)
+logging.debug(units)
 logging.debug(units.dtypes)
 
 
@@ -215,7 +246,7 @@ samples["group"] = np.nan
 samples["condition"] = args.condition
 samples.drop_duplicates(inplace=True)
 samples.sort_values(by=["sample"], inplace=True)
-logging.info(samples)
+logging.debug(samples)
 
 
 # Define grouping
@@ -243,29 +274,19 @@ else:
     datasets.append((out_path, samples, units))
 
 
-datasets_keep = []
 for out_path, sample, units in sorted(datasets):
-    units_small = [
-        Path(data.format(Read=1)).stat().st_size < args.min_file_size * 1024
-        for data in units["data"]
-    ]
-    if all(units_small):
+    file_sizes_kb = [round(Path(data.format(Read=1)).stat().st_size / 1024, 1) for data in units["data"]]
+    if any(file_size_kb < args.min_file_size for file_size_kb in file_sizes_kb):
+        total_reads = [int(gzip_n_lines(data.format(Read=1))/4) for data in units["data"]]
         print(
-            f"#{out_path.parent} # - All R1 files smaller than {args.min_file_size} KiB"
-        )
-    elif any(units_small):
-        print(
-            f"#{out_path.parent} # - At least one R1 file smaller than {args.min_file_size} KiB"
+            f"{out_path.parent}\t# Total reads: {total_reads}; File size (KiB): {file_sizes_kb};"
         )
     else:
         print(out_path.parent)
-        datasets_keep.append((out_path, sample, units))
-
-logging.debug(datasets_keep)
 
 
 # Check if groups exist
-datasets_exist = [out_path.is_dir() for out_path, _, _ in datasets_keep]
+datasets_exist = [out_path.is_dir() for out_path, _, _ in datasets]
 if any(datasets_exist):
     logging.warning(f"Some of the datasets already exist ({datasets_exist})!")
 
@@ -274,7 +295,7 @@ if any(datasets_exist):
 if args.dryrun:
     logging.debug("Dry-run finished successfully.")
 else:
-    for out_path, samples, units in datasets_keep:
+    for out_path, samples, units in datasets:
         if out_path.exists() and not args.force:
             logging.warning(f"Unit {out_path} already exists. Skipping!")
             continue
