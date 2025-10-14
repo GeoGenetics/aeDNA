86c6ec10fbe207c65a41063d311e9611bb537d72
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-10-14 14:24:02 +0200

    Add unit testing

---
+++ examples/euk_prok_virus/.tests/unit/test_taxon_prefilter_shard_bowtie2.py
@@ -0,0 +1,60 @@
+"""
+Rule test code for unit testing of rules generated with Snakemake 9.13.2.
+"""
+
+
+import os
+import sys
+import shutil
+import tempfile
+from pathlib import Path
+from subprocess import check_output
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def test_taxon_prefilter_shard_bowtie2(conda_prefix):
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        config_path = Path(".tests/unit/taxon_prefilter_shard_bowtie2/config")
+        data_path = Path(".tests/unit/taxon_prefilter_shard_bowtie2/data")
+        expected_path = Path(".tests/unit/taxon_prefilter_shard_bowtie2/expected")
+
+        # Copy config to the temporary workdir.
+        shutil.copytree(config_path, workdir)
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir, dirs_exist_ok=True)
+
+        # Run the test job.
+        check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "temp/prefilter_shards/bowtie2/Lib_LVsim1_collapsed.virus.1-of-1.bam",
+                "--snakefile",
+                "../../workflow/Snakefile",
+                "-f",
+                "--notemp",
+                "--show-failed-logs",
+                "-j1",
+                "-c2",
+                "--target-files-omit-workdir-adjustment",
+                "--configfile",
+                "config/config.yaml",
+                "--software-deployment-method",
+                "conda",
+                "--directory",
+                workdir,
+            ]
+            + conda_prefix
+        )
+
+        # Check the output byte by byte using cmp/zmp/bzcmp/xzcmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        import common
+        common.OutputChecker(data_path, expected_path, workdir).check()
