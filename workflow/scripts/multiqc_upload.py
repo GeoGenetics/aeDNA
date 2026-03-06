017b0d72d1a5fef4175d33a704c266da48e60d6d
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-03-06 11:53:57 +0100

    Run QC upload on tests

--- workflow/scripts/multiqc_upload.py
+++ workflow/scripts/multiqc_upload.py
@@ -108,15 +108,10 @@ def main():
     logging.debug(args)
 
     # Create SQLAlchemy engine
-    import os
-    from urllib.parse import urlparse
     from sqlalchemy import create_engine
     from sqlalchemy_utils import database_exists, create_database, drop_database
 
-    url = urlparse(args.db_url)
-    username = os.environ.get("SQL_USER", url.username)
-    password = os.environ.get("SQL_PASSWORD", url.password)
-    engine = create_engine(url._replace(netloc=f'{username}:{password}@{url.hostname}:{url.port or 5432}').geturl())
+    engine = create_engine(args.db_url)
 
     # Checking if DB exists
     if database_exists(engine.url):
